dddc081d93ef6380c588ef82d6209599ede95cb2
cacalo <cacalo219@hotmail.com>
2024-08-28 12:58:22 -0300

    Fin clase de pantalla login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { EstadoCocherasComponent } from './pages/estado-cocheras/estado-cocheras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "estado-cocheras",
+        component: EstadoCocherasComponent
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
