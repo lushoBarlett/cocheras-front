24de5f1ee2217a8c2425c4e10b5f9086c5a68a79
cacalo <cacalo219@hotmail.com>
2024-09-04 12:56:16 -0300

    Fin clase de tipos de datos, listas y @for

--- src/app/pages/estado-cocheras/estado-cocheras.component.ts
+++ src/app/pages/estado-cocheras/estado-cocheras.component.ts
@@ -8,6 +8,24 @@ import { RouterModule } from '@angular/router';
   templateUrl: './estado-cocheras.component.html',
   styleUrl: './estado-cocheras.component.scss'
 })
-export class EstadoCocherasComponent {
 
+export class EstadoCocherasComponent {
+  titulo: string = 'Estado de la cochera';
+  header: { nro: string, disponibilidad: string, ingreso: string, acciones: string } = {
+    nro: 'Nro',
+    disponibilidad: 'Disponibilidad',
+    ingreso: 'Ingreso',
+    acciones: 'Acciones',
+  };
+  filas: { nro: number, disponibilidad: boolean, ingreso: string, acciones: string }[] = [];
+  siguienteNumero: number = 1;
+  agregarFila() {
+    this.filas.push({
+      nro: this.siguienteNumero,
+      disponibilidad: true,
+      ingreso: new Date().toLocaleTimeString(),
+      acciones: '-'
+    });
+    this.siguienteNumero += 1;
+  }
 }
